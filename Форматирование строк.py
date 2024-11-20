56f254c2b3346e9fb4d172a9a4f49c51435c223a
Nastya-Mart <nastya.mart749@gmail.com>
2024-11-20 19:00:54 +0300

    Add files via upload

---
+++ Форматирование строк.py
@@ -0,0 +1,21 @@
+team1_num = 5
+print('В команде Мастера кода участников: %s!' % team1_num)
+
+team2_num = 6
+print('Итого сегодня в командах участников: %s и %s!' %(team1_num, team2_num))
+
+score_2 = 42
+print('Команда Волшебники данных решила задач:{}!'.format(score_2))
+
+team1_time = 18015.2
+print('Волшебники данных решили задачи за {}c!' .format(team1_time))
+
+score_1 = 40
+print('Команды решили {} и {} задач.' .format(score_1, score_2))
+
+challenge_result = 'Мастера кода!'
+print('Результат битвы: победа команды {}'.format(challenge_result))
+
+tasks_total = 82
+time_avg = 350.4
+print('Сегодня было решено {} задачи, в среднем по {} секунды на задачу!.'.format(tasks_total, time_avg))
